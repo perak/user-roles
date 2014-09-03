bb726111266a87a0518dcff47513a845aa043f73
Petar Korponaić <petar.korponaic@gmail.com>
2014-09-03 14:56:52 +0200

    Changed package.js

--- package.js
+++ package.js
@@ -5,11 +5,11 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-	api.add_files('users/both/collections/users.js', ['client', 'server']);
-	api.add_files('users/client/helpers.js', 'client');
-	api.add_files('users/server/collections/users.js', 'server');
-	api.add_files('users/server/publications/admin_user.js', 'server');
-	api.add_files('users/server/publications/admin_users.js', 'server');
-	api.add_files('users/server/publications/current_user_data.js', 'server');
+	api.add_files('both/collections/users.js', ['client', 'server']);
+	api.add_files('client/helpers.js', 'client');
+	api.add_files('server/collections/users.js', 'server');
+	api.add_files('server/publications/admin_user.js', 'server');
+	api.add_files('server/publications/admin_users.js', 'server');
+	api.add_files('server/publications/current_user_data.js', 'server');
 	api.export('Users');
 });
