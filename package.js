8f12d3a9f21ba460a875f8f55103276099336661
Petar Korponaić <petar.korponaic@gmail.com>
2014-10-03 10:32:49 +0200

    Added 'users-null' used by meteor-kitchen generator

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	summary: "User roles package for Meteor",
-	version: "1.0.8",
+	version: "1.0.9",
 	git: "https://github.com/perak/user-roles.git"
 });
 
@@ -8,6 +8,7 @@ Package.describe({
 if(!Package.onUse) Package.onUse = Package.on_use;
 
 Package.onUse(function(api) {
+	// Meteor >= 0.9?
 	if(api.versionsFrom) api.versionsFrom('METEOR@0.9.0');
 
 	api.use('underscore');
